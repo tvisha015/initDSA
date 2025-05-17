beeb610d9b49fd4a10a107e1bee6b379b0bcbed1
Tvisha Sharma <tvisha.edu@gmail.com>
2025-05-17 23:25:04 +0530

    Added delete-problem controller

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -220,6 +220,39 @@ export const updateProblem = async(req, res)=>{
     }
 }
 
-export const deleteProblem = async(req, res)=>{}
+export const deleteProblem = async(req, res)=>{
+    const {id} = req.params;
+
+    try {
+        const problem = await db.problem.findUnique({
+            where:{
+                id
+            }
+        });
+
+        if(!problem){
+            return res.status(404).json({
+                error:"Problem not found"
+            });
+        }
+
+        await db.problem.delete({
+            where:{
+                id
+            }
+        });
+
+        res.status(200).json({
+            success:true,
+            message:"Problem deleted successfully",
+        })
+    } catch (error) {
+        console.error('Error deleting problem:', error);
+        res.status(500).json({ 
+            success: false,
+            error: 'Failed to delete problem' 
+        });
+    }
+}
 
 export const getAllProblemsSolvedByUser = async(req, res)=>{}
