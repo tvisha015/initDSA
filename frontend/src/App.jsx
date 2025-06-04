13e119c9a142eef901fb3cf7c4343965ac9e7300
Tvisha Sharma <tvisha.edu@gmail.com>
2025-06-04 23:09:38 +0530

    Initialized frontend, implemented login and signup page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import {Routes, Route, Navigate} from "react-router-dom"
-import HomePage from './page/HomePage';
-import { LoginPage } from './page/LoginPage';
-import { SignUpPage } from './page/SignUpPage';
+import HomePage from './page/HomePage.jsx';
+import { LoginPage } from './page/LoginPage.jsx';
+import { SignUpPage } from './page/SignUpPage.jsx';
 
 const App = () => {
+
+  let authUser = null;
+
   return (
     <div className='flex flex-col items-center justify-center'>
       <Routes>
 
         <Route
         path='/'
-        element={<HomePage/>}
+        element={authUser ? <HomePage/> : <Navigate to={"/signUp"}/>}
         />
 
         <Route
         path='/login'
-        element={<LoginPage/>}
+        element={!authUser ? <LoginPage/> : <Navigate to={"/"}/>}
         />
 
         <Route
         path='/signUp'
-        element={<SignUpPage/>}
+        element={!authUser ? <SignUpPage/> : <Navigate to={"/"}/>}
         />
       </Routes>
     </div>
