f130f0b48bfcfa2e807b2122ebd413c8cb8f4dc4
Tvisha Sharma <tvisha.edu@gmail.com>
2025-06-04 11:34:25 +0530

    Structured backend and frontend folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Routes, Route, Navigate} from "react-router-dom"
+import HomePage from './page/HomePage';
+import LoginPage from './page/LoginPage';
+import SignUpPage from './page/SignUpPage';
+const App = () => {
+  return (
+    <div className='flex flex-col items-center justify-center'>Welcome to TheCodeLab
+      <Routes>
+
+        <Route
+        path='/'
+        element={<HomePage/>}
+        />
+
+        <Route
+        path='/login'
+        element={<LoginPage/>}
+        />
+
+        <Route
+        path='/signUp'
+        element={<SignUpPage/>}
+        />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
