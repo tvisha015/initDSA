5e6c0294b74b39a877599fd53ff32b2d7ce08338
Tvisha Sharma <tvisha.edu@gmail.com>
2025-06-07 14:44:49 +0530

    Changes cors value

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use("/api-docs", swaggerUIPath.serve, swaggerUIPath.setup(swaggerjsonFilePath));
 app.use(
     cors({
-        origin:["http://localhost:5173","https://init-dsa.vercel.app","https://initdsa.in","http://initdsa.in"],
+        origin:["*"],
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     })
