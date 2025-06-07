ae116854011ad2740374b9555c9802fad4419f38
Tvisha Sharma <tvisha.edu@gmail.com>
2025-06-07 15:15:55 +0530

    Removed origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,13 +24,6 @@ const allowedOrigins = [
 ];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
